c30b53b520889ff932e561ffc3194cd0e5c870ac
Jasmine Zhang <jasminezhang@live.com>
2019-08-30 12:01:58 -0700

    3x3 grid, drag and snap

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    float distance;
+    bool dragging;
+
+    Transform transform;
+
+    private void Start() {
+        transform = gameObject.transform;
+    }
+
+    private void OnMouseDown() {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    private void OnMouseUp() {
+        dragging = false;
+    }
+
+    private void Update() {
+        if (!dragging)
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 rayPoint = ray.GetPoint(distance);
+        transform.position = rayPoint;
+
+        //Snap();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (dragging)
+            return;
+
+        Snap(collision.gameObject);
+    }
+
+    private void Snap(GameObject obj) {
+        var currentPos = obj.transform.position;
+        transform.position = new Vector3(Mathf.Round(currentPos.x),
+                                     Mathf.Round(currentPos.y),
+                                     Mathf.Round(currentPos.z));
+    }
+
+    private void Snap() {
+        var currentPos = transform.position;
+        transform.position = new Vector3(Mathf.Round(currentPos.x),
+                                     Mathf.Round(currentPos.y),
+                                     Mathf.Round(currentPos.z));
+    }
+}
---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpace : MonoBehaviour
+{
+    public Vector3 center;
+
+    private void Start() {
+        center = transform.position;
+    }
+}
