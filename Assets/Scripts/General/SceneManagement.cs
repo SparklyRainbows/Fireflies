2e3961055416830b49f80aac78cecff14e3732e1
Jasmine Zhang <jasminezhang@live.com>
2019-09-02 09:55:42 -0700

    added three levels

--- Assets/Scripts/General/SceneManagement.cs
+++ Assets/Scripts/General/SceneManagement.cs
@@ -1,10 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+    List<Action> levelList;
+
+    private void Start() {
+        levelList = new List<Action>() {
+            () => ThreeA(),
+            () => ThreeB(),
+            () => ThreeC(),
+            () => FourA(),
+            () => FourB(),
+            () => FiveA(),
+            () => SevenA()
+        };
+    }
+
     public void LoadNextLevel() {
         LoadLevel(GameControl.instance.level);
     }
@@ -28,23 +43,7 @@ public class SceneManagement : MonoBehaviour
     public void LoadLevel(int level) {
         GameControl.instance.level = level;
 
-        switch(level) {
-            case (1):
-                LevelOne();
-                break;
-            case (2):
-                LevelTwo();
-                break;
-            case (3):
-                LevelThree();
-                break;
-            case (4):
-                LevelFour();
-                break;
-            default:
-                GameControl.instance.WinGame();
-                return;
-        }
+        levelList[level - 1].Invoke();
 
         LoadLevel();
     }
@@ -55,7 +54,7 @@ public class SceneManagement : MonoBehaviour
 
     //---------------------
 
-    private void LevelOne() {
+    private void ThreeA() {
         List<ColorName> fireflies = new List<ColorName>() {
             ColorName.BLUE,
             ColorName.RED,
@@ -76,7 +75,7 @@ public class SceneManagement : MonoBehaviour
         GameControl.instance.LoadLevel(3, fireflies, targetColors, targetLocations);
     }
 
-    private void LevelTwo() {
+    private void ThreeB() {
         List<ColorName> fireflies = new List<ColorName>() {
             ColorName.BLUE,
             ColorName.BLUE,
@@ -95,7 +94,74 @@ public class SceneManagement : MonoBehaviour
         GameControl.instance.LoadLevel(3, fireflies, targetColors, targetLocations);
     }
 
-    private void LevelThree() {
+    private void ThreeC() {
+        List<ColorName> fireflies = new List<ColorName>() {
+            ColorName.RED,
+            ColorName.YELLOW,
+            ColorName.BLUE
+        };
+
+        List<ColorName> targetColors = new List<ColorName>() {
+            ColorName.VIOLET,
+            ColorName.BROWN,
+            ColorName.GREEN
+        };
+        List<int> targetLocations = new List<int>() {
+            2,
+            3,
+            8
+        };
+
+        GameControl.instance.LoadLevel(3, fireflies, targetColors, targetLocations);
+    }
+
+    private void FourA() {
+        List<ColorName> fireflies = new List<ColorName>() {
+            ColorName.RED,
+            ColorName.YELLOW,
+            ColorName.BLUE,
+            ColorName.BLUE
+        };
+
+        List<ColorName> targetColors = new List<ColorName>() {
+            ColorName.VIOLET,
+            ColorName.BLUE,
+            ColorName.GREEN
+        };
+        List<int> targetLocations = new List<int>() {
+            6,
+            8,
+            15
+        };
+
+        GameControl.instance.LoadLevel(4, fireflies, targetColors, targetLocations);
+    }
+
+    private void FourB() {
+        List<ColorName> fireflies = new List<ColorName>() {
+            ColorName.ORANGE,
+            ColorName.YELLOW,
+            ColorName.BLUE,
+            ColorName.VIOLET
+        };
+
+        List<ColorName> targetColors = new List<ColorName>() {
+            ColorName.VIOLET,
+            ColorName.BROWN,
+            ColorName.GREEN,
+            ColorName.ORANGE
+        };
+        List<int> targetLocations = new List<int>() {
+            1,
+            5,
+            13,
+            15
+        };
+
+        GameControl.instance.LoadLevel(4, fireflies, targetColors, targetLocations);
+    }
+
+    private void FiveA() {
         List<ColorName> fireflies = new List<ColorName>() {
             ColorName.VIOLET,
             ColorName.RED,
@@ -123,7 +189,7 @@ public class SceneManagement : MonoBehaviour
         GameControl.instance.LoadLevel(5, fireflies, targetColors, targetLocations);
     }
 
-    private void LevelFour() {
+    private void SevenA() {
         List<ColorName> fireflies = new List<ColorName>() {
             ColorName.RED,
             ColorName.ORANGE,
